7e9fc31502fe9f8049ba2acc893e71cc978f76a2
Mladen Makivic <53306430+Makivic1203@users.noreply.github.com>
2021-12-14 18:53:22 -0500

    wrapping up

--- server.js
+++ server.js
@@ -1,6 +1,3 @@
-// const fs = require('fs');
-// const path = require('path');
-
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
@@ -8,12 +5,11 @@ const app = express();
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
-// parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
-// parse incoming JSON data
 app.use(express.json());
 app.use(express.static('public'));
 
+// Use apiRoutes
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
